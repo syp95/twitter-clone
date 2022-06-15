4aa2d44fcae2a9c4ea0a254b9b84e4bc02923236
syp95 <sy95_055@naver.com>
2022-06-15 14:01:52 +0900

    get tweet from firebase

--- src/index.js
+++ src/index.js
@@ -5,8 +5,4 @@ import App from './components/App';
 import firebase from './fbase';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-);
+root.render(<App />);
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,16 +1,33 @@
 import { dbService } from 'fbase';
-import { addDoc, collection } from 'firebase/firestore';
-import React, { useState } from 'react';
+import { addDoc, collection, getDocs, query, where } from 'firebase/firestore';
+import React, { useEffect, useState } from 'react';
 
 const Home = () => {
     const [tweet, setTweet] = useState('');
+    const [tweets, setTweets] = useState([]);
+
+    const getTweets = async () => {
+        const dbTweets = await getDocs(collection(dbService, 'tweet'));
+
+        dbTweets.forEach((doc) => {
+            const tweetObj = {
+                ...doc.data(),
+                id: doc.id,
+            };
+            console.log(tweetObj);
+            setTweets((prev) => [tweetObj, ...prev]);
+        });
+    };
+    useEffect(() => {
+        getTweets();
+    }, []);
+
     const onTweetSubmit = (event) => {
         event.preventDefault();
         addDoc(collection(dbService, 'tweet'), {
             tweet,
             createdAt: Date.now(),
         });
-
         setTweet('');
     };
     const onChange = (event) => {
