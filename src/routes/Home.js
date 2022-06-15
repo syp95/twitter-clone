95007b16c71aae36e1a676f78dfe492c9a0d9cad
syp95 <sy95_055@naver.com>
2022-06-16 00:16:33 +0900

    upload image firestorage in progress

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,6 @@
 import Tweet from 'components/Tweet';
-import { dbService } from 'fbase';
+import { dbService, storageService } from 'fbase';
+import { v4 as uuidv4 } from 'uuid';
 import {
     addDoc,
     collection,
@@ -10,12 +11,13 @@ import {
     query,
     where,
 } from 'firebase/firestore';
+import { ref, uploadString } from 'firebase/storage';
 import React, { useEffect, useState } from 'react';
 
 const Home = ({ userObj }) => {
     const [tweet, setTweet] = useState('');
     const [tweets, setTweets] = useState([]);
-
+    const [image, setImage] = useState('');
     useEffect(() => {
         const q = query(
             collection(dbService, 'tweet'),
@@ -37,16 +39,33 @@ const Home = ({ userObj }) => {
         setTweet(value);
     };
 
-    const onTweetSubmit = (event) => {
+    const onTweetSubmit = async (event) => {
         event.preventDefault();
-        addDoc(collection(dbService, 'tweet'), {
-            text: tweet,
-            createdAt: Date.now(),
-            creatorId: userObj.uid,
-        });
-        setTweet('');
-    };
+        const fileRef = ref(storageService, `${userObj.uid}/${uuidv4()}`);
+        const response = await uploadString(fileRef, image, 'data_url');
+        console.log(response);
 
+        // addDoc(collection(dbService, 'tweet'), {
+        //     text: tweet,
+        //     createdAt: Date.now(),
+        //     creatorId: userObj.uid,
+        // });
+        // setTweet('');
+    };
+    const onFileChange = (event) => {
+        const {
+            target: { files },
+        } = event;
+        const theFile = files[0];
+        const reader = new FileReader();
+        reader.onloadend = (finish) => {
+            setImage(finish.currentTarget.result);
+        };
+        reader.readAsDataURL(theFile);
+    };
+    const onImageClear = () => {
+        setImage(null);
+    };
     return (
         <div>
             <form onSubmit={onTweetSubmit}>
@@ -58,6 +77,13 @@ const Home = ({ userObj }) => {
                     onChange={onChange}
                 />
                 <input type='submit' value='tweet' />
+                {image && (
+                    <div>
+                        <img style={{ width: '100px' }} src={image} />
+                        <button onClick={onImageClear}>Clear</button>
+                    </div>
+                )}
+                <input type='file' accept='image/*' onChange={onFileChange} />
             </form>
             <ul>
                 {tweets.map((data) => (
